8df90de5f88a9769739eaf8e8f2ffaac028d5ad9
michelbsilva <michelbsilva94@gmail.com>
2021-11-29 16:07:02 -0300

    Iniciando projeto

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Builder</title>
+    <link rel="stylesheet" href="_cdn/css/bootstrap_person.css">
+    <link rel="stylesheet" href="_cdn/css/app.css">
+</head>
+<body>
+    
+<header class="main_header">
+    
+    <div class="header_bar bg-front">
+        <div class="container">
+            <dix class="row">
+                <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                    <i class="icon-location-arrow"></i>
+                    <p class="my-auto ml-3">Avenida Pequeno Principe, 0 Campeche Florianópolis/SC</p>
+                </div>
+            </dix>
+        </div>
+    </div>
+
+</header>
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
