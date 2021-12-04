efdd79259075c7abb515429c7099d156cba622e4
michelbsilva <michelbsilva94@gmail.com>
2021-12-04 17:18:10 -0300

    home

--- index.php
+++ index.php
@@ -33,7 +33,6 @@
         </dix>
       </div>
     </div>
-
 <nav class="navbar navbar-expand-md navbar-light my-3">
   <div class="container">
     <div class="navbar-brand">
@@ -56,12 +55,21 @@
     </div>
   </div>
 </nav>
-    
   </header>
+
+  <?php
+    $getApp = filter_input(INPUT_GET, 'app',FILTER_SANITIZE_STRIPPED);
+    if(empty($getApp)){
+      require 'widget/home.php';
+    }elseif(!empty($getApp) && file_exists('widget/'. $getApp . '.php')){
+      require 'widget/'. $getApp . '.php';
+    }else {
+      'Oooops, erro no app informado!';
+    }
+  ?>
 
   <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <script src="node_modules/jquery/dist/jquery.min.js"></script>
-  
-</body>
 
+</body>
 </html>
---
+++ widget/home.php
@@ -0,0 +1,11 @@
+<div class="main_slide">
+    <div class="container" style="height: 100%;">
+        <div class="row align-items-center" style="height: 100%;">
+            <div class="col-8">
+                <p class="main_slide_content">Encontre o <b>imóvel ideal</b> para você e <b>sua família</b> morar  na praia!</p>
+                <a href="#" class="btn btn-front btn-lg">Quero <b>Alugar!</b></a>
+                <a href="#" class="btn btn-front btn-lg">Quero <b>Comprar!</b></a>
+            </div>
+        </div>
+    </div>
+</div>
